ccb196065355133ad810fe12f49f580a38273eb5
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-09-11 11:40:50 +0800

    Update init.js

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -3,7 +3,7 @@ import FeaturedUserBanner from "../components/featured-user-banner.gjs";
 import { defaultHomepage } from "discourse/lib/utilities";
 
 export default apiInitializer((api) => {
-  api.onPageChange((url) => {
+  api.onPageChange((url, arg) => {
     console.log(url);
     api.renderInOutlet("above-main-container", FeaturedUserBanner);
   });
