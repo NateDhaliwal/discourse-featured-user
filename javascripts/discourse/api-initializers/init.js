31d1b78f3fa359f1f6ba2c965e527a4e8984d1fe
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-09-11 11:40:02 +0800

    Update init.js

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -1,6 +1,10 @@
 import { apiInitializer } from "discourse/lib/api";
 import FeaturedUserBanner from "../components/featured-user-banner.gjs";
+import { defaultHomepage } from "discourse/lib/utilities";
 
 export default apiInitializer((api) => {
-  api.renderInOutlet("above-main-container", FeaturedUserBanner);
+  api.onPageChange((url) => {
+    console.log(url);
+    api.renderInOutlet("above-main-container", FeaturedUserBanner);
+  });
 });
