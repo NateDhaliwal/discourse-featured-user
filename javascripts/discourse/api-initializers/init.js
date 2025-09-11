2e64844ceb27c68b5f09c06065e0919bb87b8014
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-09-11 11:43:35 +0800

    Update init.js

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -3,10 +3,8 @@ import FeaturedUserBanner from "../components/featured-user-banner.gjs";
 import { defaultHomepage } from "discourse/lib/utilities";
 
 export default apiInitializer((api) => {
-  api.onPageChange((url, arg) => {
-    console.log(url);
-    console.log(arg);
-    console.log(defaultHomepage());
+  const router = api.container.lookup("service:router");
+  if (router.currentRouteName === `discovery.${defaultHomepage()}`) {
     api.renderInOutlet("above-main-container", FeaturedUserBanner);
-  });
+  }
 });
