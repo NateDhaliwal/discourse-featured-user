ec9cfb471452b632aafdcec156bdaee08688741f
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-09-11 11:48:02 +0800

    Update init.js

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -4,10 +4,12 @@ import { defaultHomepage } from "discourse/lib/utilities";
 
 export default apiInitializer((api) => {
   const router = api.container.lookup("service:router");
+  const { currentRouteName } = router;
   console.log(router);
   console.log(`discovery.${defaultHomepage()}`);
-  console.log(router.currentRouteName === `discovery.${defaultHomepage()}`);
-  if (router.currentRouteName === `discovery.${defaultHomepage()}`) {
+  console.log(currentRouteName);
+  console.log(currentRouteName === `discovery.${defaultHomepage()}`);
+  if (currentRouteName === `discovery.${defaultHomepage()}`) {
     api.renderInOutlet("above-main-container", FeaturedUserBanner);
   }
 });
